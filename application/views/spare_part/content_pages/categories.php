8aec0d176ed4f221c28ac6bb3bd2f78216c8f36d
randyhesh <heshani7.herath@gmail.com>
2015-11-08 21:33:22 +0530

    home content divided

---
+++ application/views/spare_part/content_pages/categories.php
@@ -0,0 +1,322 @@
+<div class="items-list mCustomScrollbar _mCS_1">
+    <div class="inner">
+        <header>
+            <h3>All Products</h3>
+        </header>
+        <ul class="results list">
+            <li>
+                <div id="1" class="item">
+                    <a class="image loaded" href="#">
+                        <div class="inner">
+                            <div class="item-specific">
+                                <span title="Bedrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bedrooms.png">
+                                    2
+                                </span>
+                                <span title="Bathrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bathrooms.png">
+                                    2
+                                </span>
+                                <span title="Area">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/area.png">
+                                    240
+                                    <sup>2</sup>
+                                </span>
+                                <span title="Garages">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/garages.png">
+                                    1
+                                </span>
+                            </div>
+                            <img class="mCS_img_loaded" alt="" src="http://themestarz.net/html/spotter/assets/img/items/1.jpg">
+                        </div>
+                    </a>
+                    <div class="wrapper">
+                        <a id="1" href="#">
+                            <h3>Service Parts</h3>
+                        </a>
+                        <figure>63 Birch Street</figure>
+                        <div class="price">$2500</div>
+                        <div class="info">
+                            <div class="type">
+                                <i>
+                                    <img class="mCS_img_loaded" alt="" src="assets/icons/store/apparel/umbrella-2.png">
+                                </i>
+                                <span>Apartment</span>
+                            </div>
+                            <div class="rating" data-rating="4">
+                                <span class="stars">
+                                    <i class="fa fa-star s1 active" data-score="1"></i>
+                                    <i class="fa fa-star s2 active" data-score="2"></i>
+                                    <i class="fa fa-star s3 active" data-score="3"></i>
+                                    <i class="fa fa-star s4 active" data-score="4"></i>
+                                    <i class="fa fa-star s5" data-score="5"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div id="1" class="item">
+                    <a class="image loaded" href="#">
+                        <div class="inner">
+                            <div class="item-specific">
+                                <span title="Bedrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bedrooms.png">
+                                    2
+                                </span>
+                                <span title="Bathrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bathrooms.png">
+                                    2
+                                </span>
+                                <span title="Area">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/area.png">
+                                    240
+                                    <sup>2</sup>
+                                </span>
+                                <span title="Garages">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/garages.png">
+                                    1
+                                </span>
+                            </div>
+                            <img class="mCS_img_loaded" alt="" src="http://themestarz.net/html/spotter/assets/img/items/1.jpg">
+                        </div>
+                    </a>
+                    <div class="wrapper">
+                        <a id="1" href="#">
+                            <h3>Brakes</h3>
+                        </a>
+                        <figure>63 Birch Street</figure>
+                        <div class="price">$2500</div>
+                        <div class="info">
+                            <div class="type">
+                                <i>
+                                    <img class="mCS_img_loaded" alt="" src="assets/icons/store/apparel/umbrella-2.png">
+                                </i>
+                                <span>Apartment</span>
+                            </div>
+                            <div class="rating" data-rating="4">
+                                <span class="stars">
+                                    <i class="fa fa-star s1 active" data-score="1"></i>
+                                    <i class="fa fa-star s2 active" data-score="2"></i>
+                                    <i class="fa fa-star s3 active" data-score="3"></i>
+                                    <i class="fa fa-star s4 active" data-score="4"></i>
+                                    <i class="fa fa-star s5" data-score="5"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div id="1" class="item">
+                    <a class="image loaded" href="#">
+                        <div class="inner">
+                            <div class="item-specific">
+                                <span title="Bedrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bedrooms.png">
+                                    2
+                                </span>
+                                <span title="Bathrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bathrooms.png">
+                                    2
+                                </span>
+                                <span title="Area">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/area.png">
+                                    240
+                                    <sup>2</sup>
+                                </span>
+                                <span title="Garages">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/garages.png">
+                                    1
+                                </span>
+                            </div>
+                            <img class="mCS_img_loaded" alt="" src="http://themestarz.net/html/spotter/assets/img/items/1.jpg">
+                        </div>
+                    </a>
+                    <div class="wrapper">
+                        <a id="1" href="#">
+                            <h3>Engine Parts</h3>
+                        </a>
+                        <figure>63 Birch Street</figure>
+                        <div class="price">$2500</div>
+                        <div class="info">
+                            <div class="type">
+                                <i>
+                                    <img class="mCS_img_loaded" alt="" src="assets/icons/store/apparel/umbrella-2.png">
+                                </i>
+                                <span>Apartment</span>
+                            </div>
+                            <div class="rating" data-rating="4">
+                                <span class="stars">
+                                    <i class="fa fa-star s1 active" data-score="1"></i>
+                                    <i class="fa fa-star s2 active" data-score="2"></i>
+                                    <i class="fa fa-star s3 active" data-score="3"></i>
+                                    <i class="fa fa-star s4 active" data-score="4"></i>
+                                    <i class="fa fa-star s5" data-score="5"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div id="1" class="item">
+                    <a class="image loaded" href="#">
+                        <div class="inner">
+                            <div class="item-specific">
+                                <span title="Bedrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bedrooms.png">
+                                    2
+                                </span>
+                                <span title="Bathrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bathrooms.png">
+                                    2
+                                </span>
+                                <span title="Area">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/area.png">
+                                    240
+                                    <sup>2</sup>
+                                </span>
+                                <span title="Garages">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/garages.png">
+                                    1
+                                </span>
+                            </div>
+                            <img class="mCS_img_loaded" alt="" src="http://themestarz.net/html/spotter/assets/img/items/1.jpg">
+                        </div>
+                    </a>
+                    <div class="wrapper">
+                        <a id="1" href="#">
+                            <h3>Suspension & Steering</h3>
+                        </a>
+                        <figure>63 Birch Street</figure>
+                        <div class="price">$2500</div>
+                        <div class="info">
+                            <div class="type">
+                                <i>
+                                    <img class="mCS_img_loaded" alt="" src="assets/icons/store/apparel/umbrella-2.png">
+                                </i>
+                                <span>Apartment</span>
+                            </div>
+                            <div class="rating" data-rating="4">
+                                <span class="stars">
+                                    <i class="fa fa-star s1 active" data-score="1"></i>
+                                    <i class="fa fa-star s2 active" data-score="2"></i>
+                                    <i class="fa fa-star s3 active" data-score="3"></i>
+                                    <i class="fa fa-star s4 active" data-score="4"></i>
+                                    <i class="fa fa-star s5" data-score="5"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div id="1" class="item">
+                    <a class="image loaded" href="#">
+                        <div class="inner">
+                            <div class="item-specific">
+                                <span title="Bedrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bedrooms.png">
+                                    2
+                                </span>
+                                <span title="Bathrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bathrooms.png">
+                                    2
+                                </span>
+                                <span title="Area">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/area.png">
+                                    240
+                                    <sup>2</sup>
+                                </span>
+                                <span title="Garages">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/garages.png">
+                                    1
+                                </span>
+                            </div>
+                            <img class="mCS_img_loaded" alt="" src="http://themestarz.net/html/spotter/assets/img/items/1.jpg">
+                        </div>
+                    </a>
+                    <div class="wrapper">
+                        <a id="1" href="#">
+                            <h3>Transmission</h3>
+                        </a>
+                        <figure>63 Birch Street</figure>
+                        <div class="price">$2500</div>
+                        <div class="info">
+                            <div class="type">
+                                <i>
+                                    <img class="mCS_img_loaded" alt="" src="assets/icons/store/apparel/umbrella-2.png">
+                                </i>
+                                <span>Apartment</span>
+                            </div>
+                            <div class="rating" data-rating="4">
+                                <span class="stars">
+                                    <i class="fa fa-star s1 active" data-score="1"></i>
+                                    <i class="fa fa-star s2 active" data-score="2"></i>
+                                    <i class="fa fa-star s3 active" data-score="3"></i>
+                                    <i class="fa fa-star s4 active" data-score="4"></i>
+                                    <i class="fa fa-star s5" data-score="5"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div id="1" class="item">
+                    <a class="image loaded" href="#">
+                        <div class="inner">
+                            <div class="item-specific">
+                                <span title="Bedrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bedrooms.png">
+                                    2
+                                </span>
+                                <span title="Bathrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bathrooms.png">
+                                    2
+                                </span>
+                                <span title="Area">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/area.png">
+                                    240
+                                    <sup>2</sup>
+                                </span>
+                                <span title="Garages">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/garages.png">
+                                    1
+                                </span>
+                            </div>
+                            <img class="mCS_img_loaded" alt="" src="http://themestarz.net/html/spotter/assets/img/items/1.jpg">
+                        </div>
+                    </a>
+                    <div class="wrapper">
+                        <a id="1" href="#">
+                            <h3>Cooling & Heating</h3>
+                        </a>
+                        <figure>63 Birch Street</figure>
+                        <div class="price">$2500</div>
+                        <div class="info">
+                            <div class="type">
+                                <i>
+                                    <img class="mCS_img_loaded" alt="" src="assets/icons/store/apparel/umbrella-2.png">
+                                </i>
+                                <span>Apartment</span>
+                            </div>
+                            <div class="rating" data-rating="4">
+                                <span class="stars">
+                                    <i class="fa fa-star s1 active" data-score="1"></i>
+                                    <i class="fa fa-star s2 active" data-score="2"></i>
+                                    <i class="fa fa-star s3 active" data-score="3"></i>
+                                    <i class="fa fa-star s4 active" data-score="4"></i>
+                                    <i class="fa fa-star s5" data-score="5"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <!--results-->
+</div>
---
+++ application/views/spare_part/content_pages/featured_spare_parts.php
@@ -0,0 +1,141 @@
+<div class="row">
+    <div class="col-md-3 col-sm-3">
+        <div class="item featured">
+            <div class="image">
+                <div class="quick-view" id="1"><i class="fa fa-eye"></i><span>Quick View</span></div>
+                <a href="item-detail.html">
+                    <div class="overlay">
+                        <div class="inner">
+                            <div class="content">
+                                <h4>Description</h4>
+                                <p>Curabitur odio nibh, luctus non pulvinar a, ultricies ac diam. Donec neque massa</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-specific">
+                        <span title="Bedrooms"><img src="http://themestarz.net/html/spotter/http://themestarz.net/html/spotter/assets/img/bedrooms.png" alt="">2</span>
+                        <span title="Bathrooms"><img src="http://themestarz.net/html/spotter/http://themestarz.net/html/spotter/assets/img/bathrooms.png" alt="">2</span>
+                        <span title="Area"><img src="http://themestarz.net/html/spotter/http://themestarz.net/html/spotter/assets/img/area.png" alt="">240m<sup>2</sup></span>
+                        <span title="Garages"><img src="http://themestarz.net/html/spotter/http://themestarz.net/html/spotter/assets/img/garages.png" alt="">1</span>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-thumbs-up"></i>
+                    </div>
+                    <img src="http://themestarz.net/html/spotter/http://themestarz.net/html/spotter/assets/img/items/1.jpg" alt="">
+                </a>
+            </div>
+            <div class="wrapper">
+                <a href="item-detail.html"><h3>Steak House Restaurant</h3></a>
+                <figure>63 Birch Street</figure>
+                <div class="info">
+                    <div class="type">
+                        <i><img src="assets/icons/restaurants-bars/restaurants/restaurant.png" alt=""></i>
+                        <span>Restaurant</span>
+                    </div>
+                    <div class="rating" data-rating="4"></div>
+                </div>
+            </div>
+        </div>
+        <!-- /.item-->
+    </div>
+    <!--/.col-sm-4-->
+    <div class="col-md-3 col-sm-3">
+        <div class="item featured">
+            <div class="image">
+                <div class="quick-view"><i class="fa fa-eye"></i><span>Quick View</span></div>
+                <a href="item-detail.html">
+                    <div class="overlay">
+                        <div class="inner">
+                            <div class="content">
+                                <h4>Description</h4>
+                                <p>Curabitur odio nibh, luctus non pulvinar a, ultricies ac diam. Donec neque massa</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src="http://themestarz.net/html/spotter/http://themestarz.net/html/spotter/assets/img/items/2.jpg" alt="">
+                </a>
+            </div>
+            <div class="wrapper">
+                <a href="item-detail.html"><h3>Bennyâ€™s Cafeteria</h3></a>
+                <figure>63 Birch Street</figure>
+                <div class="info">
+                    <div class="type">
+                        <i><img src="assets/icons/restaurants-bars/restaurants/cafetaria.png" alt=""></i>
+                        <span>Cafeteria</span>
+                    </div>
+                    <div class="rating" data-rating="4"></div>
+                </div>
+            </div>
+        </div>
+        <!-- /.item-->
+    </div>
+    <!--/.col-sm-4-->
+    <div class="col-md-3 col-sm-3">
+        <div class="item featured">
+            <div class="image">
+                <div class="quick-view"><i class="fa fa-eye"></i><span>Quick View</span></div>
+                <a href="item-detail.html">
+                    <div class="overlay">
+                        <div class="inner">
+                            <div class="content">
+                                <h4>Description</h4>
+                                <p>Curabitur odio nibh, luctus non pulvinar a, ultricies ac diam. Donec neque massa</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-specific">
+                        <span>Daily menu from: $6</span>
+                    </div>
+                    <img src="http://themestarz.net/html/spotter/http://themestarz.net/html/spotter/assets/img/items/restaurant/9.jpg" alt="">
+                </a>
+            </div>
+            <div class="wrapper">
+                <a href="item-detail.html"><h3>Big Bamboo</h3></a>
+                <figure>4662 Bruce Street</figure>
+                <div class="info">
+                    <div class="type">
+                        <i><img src="assets/icons/restaurants-bars/restaurants/japanese-food.png" alt=""></i>
+                        <span>Sushi</span>
+                    </div>
+                    <div class="rating" data-rating="3"></div>
+                </div>
+            </div>
+        </div>
+        <!-- /.item-->
+    </div>
+    <!--/.col-sm-4-->
+    <div class="col-md-3 col-sm-3">
+        <div class="item featured">
+            <div class="image">
+                <div class="quick-view"><i class="fa fa-eye"></i><span>Quick View</span></div>
+                <a href="item-detail.html">
+                    <div class="overlay">
+                        <div class="inner">
+                            <div class="content">
+                                <h4>Description</h4>
+                                <p>Curabitur odio nibh, luctus non pulvinar a, ultricies ac diam. Donec neque massa</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-specific">
+                        <span>Daily menu from: $6</span>
+                    </div>
+                    <img src="http://themestarz.net/html/spotter/http://themestarz.net/html/spotter/assets/img/items/restaurant/10.jpg" alt="">
+                </a>
+            </div>
+            <div class="wrapper">
+                <a href="item-detail.html"><h3>Sushi Wooshi Bar</h3></a>
+                <figure>357 Trainer Avenue</figure>
+                <div class="info">
+                    <div class="type">
+                        <i><img src="assets/icons/restaurants-bars/restaurants/fishchips.png" alt=""></i>
+                        <span>Sushi Bar</span>
+                    </div>
+                    <div class="rating" data-rating="3"></div>
+                </div>
+            </div>
+        </div>
+        <!-- /.item-->
+    </div>
+    <!--/.col-sm-4-->
+</div>
