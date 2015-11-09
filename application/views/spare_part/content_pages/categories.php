bf3cb461ff09457e25725c51d2146e374c123967
Nadeesha Liyanage <IT13009700@my.sliit.lk>
2015-11-09 00:47:04 -0800

    Spare parts Details

--- application/views/spare_part/content_pages/categories.php
+++ application/views/spare_part/content_pages/categories.php
@@ -34,9 +34,13 @@
                         <a id="1" href="#">
                             <h3>Service Parts</h3>
                         </a>
-                        <figure>63 Birch Street</figure>
-                        <div class="price">$2500</div>
-                        <div class="info">
+                        <figure>Air Filters</figure>
+                        <figure>Fuel Filters</figure>
+                        <figure>Oil Filters</figure>
+                        <figure>Spark Plugs</figure>
+                        
+<!--                        <div class="price">$2500</div>-->
+<!--                        <div class="info">
                             <div class="type">
                                 <i>
                                     <img class="mCS_img_loaded" alt="" src="assets/icons/store/apparel/umbrella-2.png">
@@ -52,7 +56,7 @@
                                     <i class="fa fa-star s5" data-score="5"></i>
                                 </span>
                             </div>
-                        </div>
+                        </div>-->
                     </div>
                 </div>
             </li>
@@ -86,8 +90,11 @@
                         <a id="1" href="#">
                             <h3>Brakes</h3>
                         </a>
-                        <figure>63 Birch Street</figure>
-                        <div class="price">$2500</div>
+                        <figure>Brake Discs</figure>
+                        <figure>Brake Drums</figure>
+                        <figure>Brake Pads</figure>
+                        <figure>Brake Shoes</figure>
+<!--                        <div class="price">$2500</div>
                         <div class="info">
                             <div class="type">
                                 <i>
@@ -104,7 +111,7 @@
                                     <i class="fa fa-star s5" data-score="5"></i>
                                 </span>
                             </div>
-                        </div>
+                        </div>-->
                     </div>
                 </div>
             </li>
@@ -138,8 +145,11 @@
                         <a id="1" href="#">
                             <h3>Engine Parts</h3>
                         </a>
-                        <figure>63 Birch Street</figure>
-                        <div class="price">$2500</div>
+                        <figure>Drive Belts</figure>
+                        <figure>Fuel Injectors</figure>
+                        <figure>Timing Belts</figure>
+                        <figure>Turbo Chargers</figure>
+<!--                        <div class="price">$2500</div>
                         <div class="info">
                             <div class="type">
                                 <i>
@@ -156,7 +166,7 @@
                                     <i class="fa fa-star s5" data-score="5"></i>
                                 </span>
                             </div>
-                        </div>
+                        </div>-->
                     </div>
                 </div>
             </li>
@@ -190,8 +200,11 @@
                         <a id="1" href="#">
                             <h3>Suspension & Steering</h3>
                         </a>
-                        <figure>63 Birch Street</figure>
-                        <div class="price">$2500</div>
+                        <figure>Bushes</figure>
+                        <figure>PAS Pumps</figure>
+                        <figure>Shock Absorbers</figure>
+                        <figure>Springs</figure>
+<!--                        <div class="price">$2500</div>
                         <div class="info">
                             <div class="type">
                                 <i>
@@ -208,7 +221,7 @@
                                     <i class="fa fa-star s5" data-score="5"></i>
                                 </span>
                             </div>
-                        </div>
+                        </div>-->
                     </div>
                 </div>
             </li>
@@ -242,8 +255,11 @@
                         <a id="1" href="#">
                             <h3>Transmission</h3>
                         </a>
-                        <figure>63 Birch Street</figure>
-                        <div class="price">$2500</div>
+                        <figure>CV Boot Kits</figure>
+                        <figure>Clutch kits</figure>
+                        <figure>Driveshafts</figure>
+                        <figure>Flywheels</figure>
+<!--                        <div class="price">$2500</div>
                         <div class="info">
                             <div class="type">
                                 <i>
@@ -260,7 +276,7 @@
                                     <i class="fa fa-star s5" data-score="5"></i>
                                 </span>
                             </div>
-                        </div>
+                        </div>-->
                     </div>
                 </div>
             </li>
@@ -294,8 +310,11 @@
                         <a id="1" href="#">
                             <h3>Cooling & Heating</h3>
                         </a>
-                        <figure>63 Birch Street</figure>
-                        <div class="price">$2500</div>
+                        <figure>Compressors</figure>
+                        <figure>Heater Matrix</figure>
+                        <figure>Radiators</figure>
+                        <figure>Thermostats</figure>
+<!--                        <div class="price">$2500</div>
                         <div class="info">
                             <div class="type">
                                 <i>
@@ -312,7 +331,172 @@
                                     <i class="fa fa-star s5" data-score="5"></i>
                                 </span>
                             </div>
+                        </div>-->
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div id="1" class="item">
+                    <a class="image loaded" href="#">
+                        <div class="inner">
+                            <div class="item-specific">
+                                <span title="Bedrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bedrooms.png">
+                                    2
+                                </span>
+                                <span title="Bathrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bathrooms.png">
+                                    2
+                                </span>
+                                <span title="Area">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/area.png">
+                                    240
+                                    <sup>2</sup>
+                                </span>
+                                <span title="Garages">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/garages.png">
+                                    1
+                                </span>
+                            </div>
+                            <img class="mCS_img_loaded" alt="" src="http://themestarz.net/html/spotter/assets/img/items/1.jpg">
                         </div>
+                    </a>
+                    <div class="wrapper">
+                        <a id="1" href="#">
+                            <h3>Electrical & Lighting</h3>
+                        </a>
+                        <figure>Alternators</figure>
+                        <figure>Batteries</figure>
+                        <figure>Bulbs</figure>
+                        <figure>Motorcycle Battery</figure>
+<!--                        <div class="price">$2500</div>
+                        <div class="info">
+                            <div class="type">
+                                <i>
+                                    <img class="mCS_img_loaded" alt="" src="assets/icons/store/apparel/umbrella-2.png">
+                                </i>
+                                <span>Apartment</span>
+                            </div>
+                            <div class="rating" data-rating="4">
+                                <span class="stars">
+                                    <i class="fa fa-star s1 active" data-score="1"></i>
+                                    <i class="fa fa-star s2 active" data-score="2"></i>
+                                    <i class="fa fa-star s3 active" data-score="3"></i>
+                                    <i class="fa fa-star s4 active" data-score="4"></i>
+                                    <i class="fa fa-star s5" data-score="5"></i>
+                                </span>
+                            </div>
+                        </div>-->
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div id="1" class="item">
+                    <a class="image loaded" href="#">
+                        <div class="inner">
+                            <div class="item-specific">
+                                <span title="Bedrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bedrooms.png">
+                                    2
+                                </span>
+                                <span title="Bathrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bathrooms.png">
+                                    2
+                                </span>
+                                <span title="Area">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/area.png">
+                                    240
+                                    <sup>2</sup>
+                                </span>
+                                <span title="Garages">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/garages.png">
+                                    1
+                                </span>
+                            </div>
+                            <img class="mCS_img_loaded" alt="" src="http://themestarz.net/html/spotter/assets/img/items/1.jpg">
+                        </div>
+                    </a>
+                    <div class="wrapper">
+                        <a id="1" href="#">
+                            <h3>Body & Exhaust</h3>
+                        </a>
+                        <figure>Body Panels</figure>
+                        <figure>Catalytic Converters</figure>
+                        <figure>Exhaust Parts</figure>
+                        <figure>Mirrors</figure>
+<!--                        <div class="price">$2500</div>
+                        <div class="info">
+                            <div class="type">
+                                <i>
+                                    <img class="mCS_img_loaded" alt="" src="assets/icons/store/apparel/umbrella-2.png">
+                                </i>
+                                <span>Apartment</span>
+                            </div>
+                            <div class="rating" data-rating="4">
+                                <span class="stars">
+                                    <i class="fa fa-star s1 active" data-score="1"></i>
+                                    <i class="fa fa-star s2 active" data-score="2"></i>
+                                    <i class="fa fa-star s3 active" data-score="3"></i>
+                                    <i class="fa fa-star s4 active" data-score="4"></i>
+                                    <i class="fa fa-star s5" data-score="5"></i>
+                                </span>
+                            </div>
+                        </div>-->
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div id="1" class="item">
+                    <a class="image loaded" href="#">
+                        <div class="inner">
+                            <div class="item-specific">
+                                <span title="Bedrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bedrooms.png">
+                                    2
+                                </span>
+                                <span title="Bathrooms">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/bathrooms.png">
+                                    2
+                                </span>
+                                <span title="Area">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/area.png">
+                                    240
+                                    <sup>2</sup>
+                                </span>
+                                <span title="Garages">
+                                    <img class="mCS_img_loaded" src="http://themestarz.net/html/spotter/assets/img/garages.png">
+                                    1
+                                </span>
+                            </div>
+                            <img class="mCS_img_loaded" alt="" src="http://themestarz.net/html/spotter/assets/img/items/1.jpg">
+                        </div>
+                    </a>
+                    <div class="wrapper">
+                        <a id="1" href="#">
+                            <h3>Lubricants & Fluids</h3>
+                        </a>
+                        <figure>Engine Oil</figure>
+                        <figure>Steering Fluid</figure>
+                        <figure>Suspension Fluid</figure>
+                        <figure>Transmission oil</figure>
+<!--                        <div class="price">$2500</div>
+                        <div class="info">
+                            <div class="type">
+                                <i>
+                                    <img class="mCS_img_loaded" alt="" src="assets/icons/store/apparel/umbrella-2.png">
+                                </i>
+                                <span>Apartment</span>
+                            </div>
+                            <div class="rating" data-rating="4">
+                                <span class="stars">
+                                    <i class="fa fa-star s1 active" data-score="1"></i>
+                                    <i class="fa fa-star s2 active" data-score="2"></i>
+                                    <i class="fa fa-star s3 active" data-score="3"></i>
+                                    <i class="fa fa-star s4 active" data-score="4"></i>
+                                    <i class="fa fa-star s5" data-score="5"></i>
+                                </span>
+                            </div>
+                        </div>-->
                     </div>
                 </div>
             </li>
