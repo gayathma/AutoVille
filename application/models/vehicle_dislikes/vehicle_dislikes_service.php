85c20fbae01d3a37805232d263371f5bace04092
randyhesh <heshani7.herath@gmail.com>
2015-11-10 14:33:43 +0530

    s

--- application/models/vehicle_dislikes/vehicle_dislikes_service.php
+++ application/models/vehicle_dislikes/vehicle_dislikes_service.php
@@ -7,10 +7,20 @@ class Vehicle_dislikes_service extends CI_Model {
         $this->load->model('vehicle_dislikes/vehicle_dislikes_model');
     }
 
+    /**
+     * insert vehicle dislike record into vehicle_dislikes table
+     * @param type $vehicle_dislikes_model
+     * @return type
+     */
     function insert_vehicle_dislike($vehicle_dislikes_model) {
         return $this->db->insert('vehicle_dislikes', $vehicle_dislikes_model);
     }
 
+    /**
+     * get vehicle dislike count from table for a given vehicle id
+     * @param type $vehicle_id
+     * @return type
+     */
     function get_dislike_count($vehicle_id) {
 
         $this->db->select('vehicle_dislikes.id,count(vehicle_dislikes.id) as dislike_count');
--- application/models/vehicle_likes/vehicle_likes_service.php
+++ application/models/vehicle_likes/vehicle_likes_service.php
@@ -7,10 +7,20 @@ class Vehicle_likes_service extends CI_Model {
         $this->load->model('vehicle_likes/vehicle_likes_model');
     }
 
+    /**
+     * insert vehicle like record into vehicle_likes table
+     * @param type $vehicle_likes_model
+     * @return type
+     */
     function insert_vehicle_like($vehicle_likes_model) {
         return $this->db->insert('vehicle_likes', $vehicle_likes_model);
     }
 
+    /**
+     * get vehicle like count from table for a given vehicle id
+     * @param type $vehicle_id
+     * @return type
+     */
     function get_like_count($vehicle_id) {
 
         $this->db->select('vehicle_likes.id,count(vehicle_likes.id) as like_count');
