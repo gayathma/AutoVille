2cd6bc1269363da4ddc8b17e9e59e4acadecedb4
ishani123 <ishanipathinayake@gmail.com>
2015-11-10 15:39:46 +0530

    spare parts back end

---
+++ backend/models/spare_parts_advertisement/spare_parts_ad_service.php
@@ -0,0 +1,47 @@
+<?php
+
+class Spare_parts_ad_service extends CI_Model{
+    
+    function __construct() {
+        parent::__construct();
+        $this->load->model('spare_parts_advertisement/spare_parts_ad_model');
+    }
+    
+    /*
+     * This is the service function to get all spare part advertisements
+     */
+    
+    public function get_all_spare_part_advertisements(){
+        
+        $this->db->select('spare_parts_advertisements.*,user.name as added_by_user,manufacture.name as manufacture,model.name as model,fuel_type.name as fuel');
+        $this->db->from('spare_parts_advertisements');
+        $this->db->join('user', 'user.id = spare_parts_advertisements.added_by');
+        $this->db->join('manufacture','manufacture.id=spare_parts_advertisements.manufacture_id');
+        $this->db->join('model','model.id=spare_parts_advertisements.model_id');
+        $this->db->join('fuel_type','fuel_type.id=spare_parts_advertisements.fuel_type_id');
+        $this->db->where('spare_parts_advertisements.is_deleted', '0');
+        $this->db->order_by("spare_parts_advertisements.added_date", "desc");
+        $query = $this->db->get();
+        return $query->result();
+    }
+    
+    /*
+     * This service function to deleted spare parts advertisements
+     */
+    
+    public function delete_spare_part_advertisements($advertisement_id){
+        $date=array('is_deleted'=>'1');
+        $this->db->where('id',$advertisement_id);
+        return $this->db->update('spare_parts_advertisements',$date);
+    }
+    
+     /*
+     * This service function is to update publish status of a advertisement
+     */
+    public function publish_spare_part_advertisement($spare_parts_advertisments_model) {
+        $data = array('is_published' => $spare_parts_advertisments_model->get_is_published());
+        $this->db->update('spare_parts_advertisements', $data, array('id' => $spare_parts_advertisments_model->get_id()));
+        return $this->db->affected_rows();
+    }
+}
+
