1055dab8ca77887109bf4de918cb28405128bac1
Gayathma <gayathma3@gmail.com>
2015-11-13 09:54:30 +0530

    back ad

--- backend/views/vehicle_advertisements/manage_advertisements_view.php
+++ backend/views/vehicle_advertisements/manage_advertisements_view.php
@@ -80,10 +80,10 @@
                         <td>
                             <?php if ($result->is_published == '1') { ?>
                                 <span class="label label-primary">Active</span>
-                                <a class="btn btn-success btn-xs"  onclick="change_advertisement_status(<?php echo $result->id; ?>, 2, this, '<?php echo $result->added_by_user; ?>', '<?php echo $result->id; ?>');"><i class="fa fa-arrow-up " title="Reject Advertisement"></i></a> 
+                                <a class="btn btn-success btn-xs"  onclick="change_advertisement_status('<?php echo $result->id; ?>', '2', this, '<?php echo $result->added_by_user; ?>', '<?php echo $result->id; ?>');"><i class="fa fa-arrow-up " title="Reject Advertisement"></i></a> 
                             <?php } elseif ($result->is_published == '0') { ?>
                                 <span class="label label-default">Pending</span> 
-                                <a class="btn btn-success btn-xs"  onclick="change_advertisement_status(<?php echo $result->id; ?>, 1, this, '<?php echo $result->added_by_user; ?>', '<?php echo $result->id; ?>');"><i class="fa fa-arrow-up " title="Approve Advertisement"></i></a> 
+                                <a class="btn btn-success btn-xs"  onclick="change_advertisement_status('<?php echo $result->id; ?>', '1', this, '<?php echo $result->added_by_user; ?>', '<?php echo $result->id; ?>');"><i class="fa fa-arrow-up " title="Approve Advertisement"></i></a> 
                             <?php } else { ?>
                                 <span class="label label-danger">Rejected</span>  
                             <?php } ?>
@@ -117,7 +117,7 @@
                 type: "POST",
                 url: site_url + '/vehicle_advertisements/delete_advertisement',
                 data: "id=" + id,
-                success: function(msg) {
+                success: function (msg) {
                     //alert(msg);
                     if (msg == 1) {
                         //document.getElementById(trid).style.display='none';
@@ -139,11 +139,25 @@
         }
 
         if (confirm(condition)) {
+
+//            $.post(site_url + '/vehicle_advertisements/change_publish_status', {id: advertisement_id, value: value, seller_id: seller_id, vehicle_id: vehicle_id}, function (msg) {
+//                if (msg == 1) {
+//                    if (value == 1) {
+//                        $(element).parent().html('<span class="label label-primary">Active</span><a class="btn btn-success btn-xs"  onclick="change_advertisement_status(' + advertisement_id + ', 2, this,' + seller_id + ',' + vehicle_id + ');"><i class="fa fa-arrow-up " title="Reject Advertisement"></i></a> ');
+//                    } else {
+//                        $(element).parent().html('<span class="label label-danger">Rejected</span> ');
+//                    }
+//
+//                } else if (msg == 2) {
+//                    alert('Error !!');
+//                }
+//            });
             $.ajax({
                 type: "POST",
                 url: site_url + '/vehicle_advertisements/change_publish_status',
-                data: "id=" + advertisement_id + "&value=" + value + '&seller_id=' + seller_id + '&vehicle_id=' + vehicle_id,
+                data: {id: advertisement_id, value: value, seller_id: seller_id, vehicle_id: vehicle_id},
                 success: function(msg) {
+                    
                     if (msg == 1) {
                         if (value == 1) {
                             $(element).parent().html('<span class="label label-primary">Active</span><a class="btn btn-success btn-xs"  onclick="change_advertisement_status(' + advertisement_id + ', 2, this,' + seller_id + ',' + vehicle_id + ');"><i class="fa fa-arrow-up " title="Reject Advertisement"></i></a> ');
@@ -163,7 +177,7 @@
     function reload_advertisements() {
         $('#advertisement_div').html('<center><div class="load-anim"><i id="animate-icon" class="fa fa-spinner fa-3x fa-spin loader-icon-margin"></i></div></center>');
         var x = $('.load-anim').show().delay(5000);
-        $.post(site_url + '/vehicle_advertisements/search_advertisements', {}, function(msg) {
+        $.post(site_url + '/vehicle_advertisements/search_advertisements', {}, function (msg) {
             $('#advertisement_div').html('');
             $('#advertisement_div').html(msg);
             x.fadeOut('slow');
