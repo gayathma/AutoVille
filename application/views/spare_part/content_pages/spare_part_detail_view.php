f029dae0a4a647cc0a4b9a4b45234bbcd8257a56
Nadeesha Liyanage <IT13009700@my.sliit.lk>
2015-11-13 10:48:13 +0530

    hhh

--- application/views/spare_part/content_pages/spare_part_detail_view.php
+++ application/views/spare_part/content_pages/spare_part_detail_view.php
@@ -133,6 +133,25 @@
 
                     </div>
                     <!-- /.col-md-8-->
+                    <div class="col-md-8 col-sm-8">
+                        <section>
+                            <h2>Quick Poll</h2>
+                            <p>Are you planning on taking advantage of any Black Friday vehicle specials? </p>
+
+                            <div class="radios">
+                                <label class="label_radio" for="radio-01">
+                                    <input name="sample-radio" id="radio-01" value="1" type="radio" checked /> Yes
+                                </label>
+                                <label class="label_radio" for="radio-02">
+                                    <input name="sample-radio" id="radio-02" value="1" type="radio" /> No
+                                </label>
+                                <label class="label_radio" for="radio-03">
+                                    <input name="sample-radio" id="radio-03" value="1" type="radio" /> Not Sure
+                                </label>
+                            </div>
+
+                        </section>
+                    </div>
                 </div>
                 <!-- /.row -->
             </section>
