e16d335621f97ab8c32e27a98a263127335e9a8f
randyhesh <heshani7.herath@gmail.com>
2015-11-15 10:39:09 +0530

    hh

--- application/views/vehicle_adds/commercial_add_view.php
+++ application/views/vehicle_adds/commercial_add_view.php
@@ -8,13 +8,13 @@
             navText: ["", ""]
         });
     });
-    
-    
+
+
 </script>
 <div class="list-slider owl-carousel">
     <?php foreach ($commercial_images as $value) { ?>
         <div class="slide">
-            <a href="<?php echo $value->url;?>" target="_blank"><img src="<?php echo base_url() . 'uploads/commercial_images/' . $value->image; ?>" height="250" width="250" alt=""></a>
+            <a href="<?php echo $value->url; ?>" target="_blank"><img src="<?php echo base_url() . 'uploads/commercial_images/' . $value->image; ?>" height="250" width="250" alt=""></a>
         </div>
     <?php } ?>
 </div>
@@ -23,10 +23,19 @@
 <br>
 <div>
     <?php foreach ($celebrity as $value) { ?>
-        <div class="slide">
-            <a><img src="<?php echo base_url() . 'uploads/celebrity/' . $value->image; ?>" height="250" width="250" alt=""></a>
-            <div>
-            <p><?php echo $value->description;?></p>
+        <div class="slide item">
+            <div class="image">
+                <a href="#">
+                    <div class="overlay">
+                        <div class="inner">
+                            <div class="content">
+                                <h4><?php echo $value->name; ?></h4>
+                                <p><?php echo $value->description; ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src="<?php echo base_url() . 'uploads/celebrity/' . $value->image; ?>" height="250" width="250" alt="" title="<?php echo $value->name; ?>">
+                </a>
             </div>
         </div>
     <?php } ?>
